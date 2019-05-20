29292de56fc38534a8bbac088e3f4f0f822e48ff
Afaque Jaya <afaquejaya123@gmail.com>
2019-05-20 06:54:00 +0530

    Added Bottom navigation

--- app/src/main/java/com/example/collegenoticeboard/ProfileActivity.java
+++ app/src/main/java/com/example/collegenoticeboard/ProfileActivity.java
@@ -2,9 +2,12 @@ package com.example.collegenoticeboard;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.BottomNavigationView;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.support.annotation.NonNull;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -15,13 +18,37 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
     private FirebaseAuth firebaseAuth;
     private TextView textViewUserEmail;
     private Button buttonLogout;
+    private TextView mTextMessage;
+
+
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_Profile:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-
+        BottomNavigationView navView = findViewById(R.id.bottom_nav_menu);
+        mTextMessage = findViewById(R.id.message);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         firebaseAuth=FirebaseAuth.getInstance();
 
         if(firebaseAuth.getCurrentUser()==null){
@@ -32,7 +59,7 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
 
         textViewUserEmail=(TextView) findViewById(R.id.textViewUserEmail);
 
-        textViewUserEmail.setText("Welcome"+user.getEmail());
+        textViewUserEmail.setText("Welcome: "+ user.getEmail());
         buttonLogout=(Button) findViewById(R.id.buttonLogout);
 
         buttonLogout.setOnClickListener(this);
